0d1f47af148d054603ddd21e2274c2c4c06c7d8a
mallickhbqa <himandribhusanmallick@gmail.com>
2016-05-31 13:15:18 +0530

    it all about learn

---
+++ src/may31_3rd_Session/ReverseOrder.java
@@ -0,0 +1,15 @@
+package may31_3rd_Session;
+
+public class ReverseOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=10;i>=0;i--)
+		{
+			System.out.println(i);
+		}
+
+
+	}
+
+}
