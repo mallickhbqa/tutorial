06834fa6d64667f37fee066ca8a19f7458141a92
mallickhbqa <himandribhusanmallick@gmail.com>
2016-05-31 13:22:18 +0530

    v2,0

--- src/may31/Hello.java
+++ src/may31/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Friends");
-
+        PrintHello.print("Hello Friends and Have a very good morning");
 	}
 
 }
