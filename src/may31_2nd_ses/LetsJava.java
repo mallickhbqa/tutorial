5ea68b264a9444572cd7dd327c07d49f5eee17ae
mallickhbqa <himandribhusanmallick@gmail.com>
2016-05-31 13:05:44 +0530

    Lets java print numbers

---
+++ src/may31_2nd_ses/LetsJava.java
@@ -0,0 +1,15 @@
+package may31_2nd_ses;
+
+public class LetsJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
